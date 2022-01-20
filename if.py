2a2b38d1d43d9eeb73fcb6fc38305e2d752b867b
pcw2925 <parkpoiuytr1@gmail.com>
2022-01-20 21:02:25 +0900

    https://github.com/pcw2925/Python

---
+++ if.py
@@ -0,0 +1,9 @@
+jeongsangsu =input("주인이 있나요? (예/아니요)")
+
+if jeongsangsu == '예' :
+    print("Check this out 나는 정상수")
+elif jeongsangsu == '아니요' :
+    print("백발백중하는 명사수")
+else:
+    print("잘못입력했습니다")
+  
